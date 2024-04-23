f86ef6ff0384e96b17fde56e843124a5c1102d12
Mohamed Amir <m.elamir027@gmail.com>
2024-04-23 22:58:03 +0200

    c101 Lecture week 2

---
+++ Level 1/CPU - Regular - L1 - C101/Lecture/Week 02/A - UOIAUAI (1).cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    if (!(c == 'a' or c == 'e' or c == 'i' or c == 'u' or c == 'o')) {
+        cout << "consonant\n";
+    } else {
+        cout << "vowel\n";
+    }
+}
