56f892653a347cd3a715c4817f0e2f54f1806c86
Abdelrahman Elzarka <108380629+Abdelrahmanelzarka@users.noreply.github.com>
2024-04-23 22:52:35 +0200

    Create Code.cpp

---
+++ Level 1/CPU - Regular - L1 - C101/Practice G/Week 01/Code.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    /// problem C
+
+   /* int N,A,B;
+    cin>>N>>A>>B;
+    cout<<N-A+B;*/
+
+    /// problem d
+    /*int a,b,c;
+    cin>>a>>b>>c;
+    int sum=(7-a)+(7-b)+(7-c);
+    cout<<sum;
+    */
+
+    /// problem e
+    /*int x,y;
+    cin>>x>>y;
+    cout<<x+y/2;*/
+
+    /// problem f
+    /*int m;
+    cin>>m;
+    cout<<48-m;*/
+
+    /// problem G
+    /*int A,B;
+    float c;
+    cin>>A>>B;
+    c=(A-B)/3.0+B;
+    cout<<c;*/
+
+
+    /// problem H
+   /* int A,B;
+    cin>>A>>B;
+    double energy=(A*B)/100.0;
+    cout<<energy;*/
+
+    /// problem I
+
+    /*int N,M;
+    cin>>N>>M;
+    cout<<(N*M)/2;*/
+
+    /// problem J
+    /*int x;
+    cin>>x;
+
+    int fives=(x+4)/5;
+    cout<<fives;*/
+
+
+    /*ceil(x*1.0/5);
+    cout<<fives;*/
+
+
+   /* if(x-fives*5>0)
+        cout<<fives+1;
+    else
+        cout<<fives;*/
+
+        /// problem K
+       /* int l,p,q;
+        cin>>l>>p>>q;
+        double time,distance;
+        time=l*1.0/(p+q);
+        distance=time*p;
+        cout<<distance;*/
+
+        /// problem L
+
+        int n;
+        cin>>n;
+        int hunderds=n/100;
+        n%=100;
+        int twenties=n/20;
+        n%=20;
+        int tens=n/10;
+        n%=10;
+        int fives=n/5;
+        n%=5;
+        int ones=n;
+        cout<<hunderds+twenties+tens+fives+ones;
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
