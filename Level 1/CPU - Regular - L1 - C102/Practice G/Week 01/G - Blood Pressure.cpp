67c351eecdd8870519988e2f768f11bc8d92a9da
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-04-25 10:42:57 +0200

    Create G - Blood Pressure.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Practice G/Week 01/G - Blood Pressure.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    float a,b,c;cin>>a>>b;
+    c=(a-b)/3+b;
+    cout<<c;
+    return 0;
+}
