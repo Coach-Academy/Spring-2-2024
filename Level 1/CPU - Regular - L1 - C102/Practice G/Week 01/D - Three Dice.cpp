ece9e0bad0be49b14eb8c1cd78c95bed26017a90
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-04-25 10:37:59 +0200

    Create D - Three Dice.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Practice G/Week 01/D - Three Dice.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int a,b,c;cin>>a>>b>>c;
+    cout<<21-a-b-c;
+    return 0;
+}
