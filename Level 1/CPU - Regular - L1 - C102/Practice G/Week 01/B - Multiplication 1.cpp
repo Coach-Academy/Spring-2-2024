a0d156d589fdf63ef5a1f7fecb0ea2b246a43c9b
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-04-25 10:35:42 +0200

    Create B - Multiplication 1.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Practice G/Week 01/B - Multiplication 1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n,w;cin>>n>>w;
+    cout<<n*w;
+    return 0;
+}
