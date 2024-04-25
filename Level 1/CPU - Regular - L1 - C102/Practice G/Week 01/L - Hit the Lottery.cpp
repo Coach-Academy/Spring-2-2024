6ac983085a3e294e15a5ff3d8abb45f097800729
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-04-25 10:47:18 +0200

    Create L - Hit the Lottery.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Practice G/Week 01/L - Hit the Lottery.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n;cin>>n;
+    int ans=n/100;
+    n%=100;
+    ans+=n/20;
+    n%=20;
+    ans+=n/10;
+    n%=10;
+    ans+=n/5;
+    n%=5;
+    ans+=n;
+    cout<<ans;
+    return 0;
+
+}
