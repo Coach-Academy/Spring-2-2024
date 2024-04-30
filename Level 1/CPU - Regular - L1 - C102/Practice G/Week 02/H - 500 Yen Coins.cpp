132e6c6b294051411cc48cafe7357bc0749a5c4b
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-04-30 12:04:38 +0300

    Create H - 500 Yen Coins.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Practice G/Week 02/H - 500 Yen Coins.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll n,x;cin>>n>>x;
+    if(n*500>=x)
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+    return 0;
+}
