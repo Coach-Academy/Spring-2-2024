09de999e9ecf95b8957989b4b1a91cff6a263e0c
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-04-30 12:01:16 +0300

    Create A - UOIAUAI.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Practice G/Week 02/A - UOIAUAI.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    char x;cin>>x;
+    if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
+    {
+        cout<<"vowel";
+    }
+    else
+    {
+        cout<<"consonant";
+    }
+    return 0;
+}
