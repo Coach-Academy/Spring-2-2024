d6c44c533a75a73010e5131e401d51206375c357
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-04-30 12:02:44 +0300

    Create D - Watermelon.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Practice G/Week 02/D - Watermelon.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int x;cin>>x;
+    if(x>2&&(x%2==0))
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+    return 0;
+}
