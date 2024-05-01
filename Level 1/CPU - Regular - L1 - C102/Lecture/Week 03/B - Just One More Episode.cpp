8ec8ace06c8d0e00f565b90aac256796bf8d7285
Mohamed Amir <m.elamir027@gmail.com>
2024-05-01 22:56:56 +0300

    Create B - Just One More Episode.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Lecture/Week 03/B - Just One More Episode.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+        if (x > 24)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}
