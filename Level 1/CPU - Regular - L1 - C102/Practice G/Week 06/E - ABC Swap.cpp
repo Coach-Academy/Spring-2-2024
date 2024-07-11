110d0217b438ae87aa60104e1c77db4eaa9b13ec
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-07-11 10:27:53 +0300

    Create E - ABC Swap.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Practice G/Week 06/E - ABC Swap.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int a,b,c;cin>>a>>b>>c;
+    swap(a,b);
+    swap(a,c);
+    cout<<a<<' '<<b<<' '<<c<<'\n';
+    return 0;
+}
