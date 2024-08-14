1da642b52cda3e5e1ae61be7270438a070cd1f53
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-08-14 15:48:36 +0300

    Create E - Currency System in Geraldion.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Practice G/Week 10/E - Currency System in Geraldion.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n;cin>>n;
+    int mn=1e5;
+    while(n--)
+    {
+        int x;cin>>x;
+        mn=min(mn,x);
+    }
+    if(mn==1) cout<<-1;
+    else cout<<1;
+    return 0;
+}
