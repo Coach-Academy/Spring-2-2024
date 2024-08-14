48d6b02a089a0cab303dbc1a7fdfadc3dd54c515
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-08-14 15:49:19 +0300

    Create G - Puzzles.cpp

---
+++ Level 1/CPU - Regular - L1 - C102/Practice G/Week 10/G - Puzzles.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int k,n;cin>>k>>n;
+    int s[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>s[i];
+    }
+    sort(s,s+n);
+    int ans=INT_MAX;
+    for(int i=k-1;i<n;i++)
+    {
+        ans=min(ans,s[i]-s[i-k+1]);
+    }
+    cout<<ans;
+    return 0;
+}
